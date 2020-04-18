ccbbca3b0b49e5e75968cd7ef4f4c72f92361e45
jwf8280031 <51179079+jwf8280031@users.noreply.github.com>
2020-04-18 21:24:03 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
+svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
